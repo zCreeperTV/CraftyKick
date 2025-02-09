8f9cfa202ef86fa9d33f3e080b22b0b2fbf31924
Michael <michaeljens09@gmail.com>
2025-02-09 13:54:51 +0100

    bug fix #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,13 @@ repositories {
     maven(
         url = "https://oss.sonatype.org/content/repositories/snapshots"
     )
+    maven("https://libraries.minecraft.net/")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.mojang:brigadier:1.2.9")
     compileOnly ("net.md-5:bungeecord-api:1.21-R0.1-SNAPSHOT")
 }
 
