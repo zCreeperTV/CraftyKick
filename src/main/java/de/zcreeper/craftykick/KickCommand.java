a8886f471063096dbf97682b25aeec8395c24cbf
Michael <michaeljens09@gmail.com>
2025-02-09 13:49:58 +0100

    Added whole Plugin

--- src/main/java/de/zcreeper/craftykick/KickCommand.java
+++ src/main/java/de/zcreeper/craftykick/KickCommand.java
@@ -6,7 +6,6 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-
 public class KickCommand extends Command {
     public KickCommand() {
         super("kick", "craftykick.command.kick");
